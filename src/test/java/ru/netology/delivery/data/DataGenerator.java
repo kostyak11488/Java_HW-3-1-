be7dfe8fd02594c3913588a78d08103c5699ad03
Kostya Yakovlev <akovlevkosta18@gmail.com>
2026-01-09 17:34:43 +0300

    initial commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,40 @@
+package ru.netology.delivery.data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private static final Random random = new Random();
+
+    private DataGenerator() {
+    }
+
+    public static UserInfo generateUser() {
+        return new UserInfo(
+                generateCity(),
+                "Иванов Иван",
+                "+79990000000"
+        );
+    }
+
+    public static String generateDate(int days) {
+        return LocalDate.now()
+                .plusDays(days)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    private static String generateCity() {
+        String[] cities = {
+                "Москва",
+                "Санкт-Петербург",
+                "Казань",
+                "Екатеринбург",
+                "Новосибирск",
+                "Самара"
+        };
+        return cities[random.nextInt(cities.length)];
+    }
+}
+
---
+++ src/test/java/ru/netology/delivery/data/UserInfo.java
@@ -0,0 +1,25 @@
+package ru.netology.delivery.data;
+
+public class UserInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
+
+    public UserInfo(String city, String name, String phone) {
+        this.city = city;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
